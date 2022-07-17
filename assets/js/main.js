912b5c7eba36dfb0c8ab5d5cecf87101f189abb5
kristinenyaga <kristine.nyaga@student.moringaschool.com>
2022-07-17 21:45:02 +0300

    makes changes to js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,29 @@
-let list = document.querySelectorAll('.link');
-        for(let i = 0;i<list.length;i++){
-            list[i].onclick = function(){
-                let j=0;
-                while(j < list.length){
-                    list[j++].className='link';
-                }
-                list[i].className='link active';
-            }
-        }
-        let toggleBar = document.querySelector('.toggleMenu');
-        let navigation = document.querySelector('.navigationBar')
+//  let list =document.querySelectorAll('.link');
+//         for(let i = 0;i<list.length;i++){
+//             list[i].onclick = function(){
+//                 let j=0;
+//                while(j < list.length){
+//                     list[j++].className='link';
+//                }
+//                 list[i].className='link active';
+//             }
+//         }
+       
+        // var a = document.getElementsByTagName('a');
+        // var t = document.createElement('input');
+        // t.type = 'text';
+        
+        // for (i = 0; i < a.length; i++) {
+        //     a[i].onclick = function() {
 
-        toggleBar.onclick = function(){
-            toggleBar.classList.toggle('active');
-            navigationBar.classList.toggle('active');
-        }
+        //         this.parentNode.appendChild(t);
+        //         return false;
+        //     };
+        // }
+function totalProduction(){
+console.log("milk production is")
+
+}
+
+let milkPerDay=document.getElementById('dailymilk')
+console.log(milkPerDay)
