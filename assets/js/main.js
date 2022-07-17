0c6bdbbf5a5af6fcfd5403317e800e6eaa8bb53c
kristinenyaga <kristine.nyaga@student.moringaschool.com>
2022-07-17 11:05:42 +0300

    adds a function that makes toggle bar responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
-let list = document.querySelectorAll('.list');
+let list = document.querySelectorAll('.link');
         for(let i = 0;i<list.length;i++){
             list[i].onclick = function(){
                 let j=0;
                 while(j < list.length){
-                    list[j++].className='list';
+                    list[j++].className='link';
                 }
-                list[i].className='list active';
+                list[i].className='link active';
             }
+        }
+        let toggleBar = document.querySelector('.toggleMenu');
+        let navigation = document.querySelector('.navigationBar')
+
+        toggleBar.onclick = function(){
+            toggleBar.classList.toggle('active');
+            navigationBar.classList.toggle('active');
         }
